ed5f61debee1bad4a8fc916fe660eb6f5fe86375
David Galeno <davidgaleno411@gmail.com>
2023-05-24 15:54:22 -0300

    opcoes-cadastro-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { CadastrarPessoaFisica } from './views/CadastrarPessoaFísica'
+import { SelectSubscribeOption } from './views/SelectSubscribeOptionScreen'
 import { LoginScreen } from './views/loginScreen/LoginScreen'
 
 function App() {
   return (
-    <LoginScreen />
+    <CadastrarPessoaFisica/>
   )
 }
 
---
+++ src/views/SelectSubscribeOptionScreen/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from "../../components/Button/Button"
+import styles from './styles.module.css'
+export const SelectSubscribeOption = () => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.titulo}>Cadastrar Como:</h1>
+            <div className={styles.buttons}>
+                <Button value="Salão de Beleza →" />
+                <Button value="Pessoa Física →" />
+            </div>
+                <Button value="Voltar ←" />
+        </div>
+    )
+}
