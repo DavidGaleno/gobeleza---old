f77734a07d8135fcc0d48781cc196c8c1de29d19
David Galeno <davidgaleno411@gmail.com>
2023-05-26 15:00:18 -0300

    begin-lista-desejos

---
+++ src/components/ListaDesejos/index.tsx
@@ -0,0 +1,43 @@
+import styles from './styles.module.css'
+import batom from '../../assets/batom.png'
+import { LoginIcon } from '../LoginIcon'
+import { Title } from '../Title'
+import { Item } from '../Item'
+import { ActionButton } from '../ActionButton'
+import listaDesejos from '../../assets/listaDesejos.svg'
+interface Props {
+    visible: boolean
+    setVisible: (visible: boolean) => void
+}
+export const ListaDesejos = ({ visible, setVisible }: Props) => {
+
+    return (
+        <>
+            <div className={`${styles.fade} ${!visible ? styles.hide : ''}`}></div>
+            <div className={`${styles.container} ${visible ? styles.visible : styles.invisible}`}>
+                <LoginIcon onClick={() => setVisible(!visible)} fatherClass={styles.icon} image={listaDesejos} alt='CarrinhoCompras' />
+                <Title value='Lista de Desejos' />
+                <div className={styles.itens}>
+
+                    <Item image={batom} alt="Batom" nome="Batom" preco={9.99} />
+                    <Item image={batom} alt="Batom" nome="Batom" preco={9.99} />
+
+                    <Item image={batom} alt="Batom" nome="Batom" preco={9.99} />
+
+                    <Item image={batom} alt="Batom" nome="Batom" preco={9.99} />
+
+                    <Item image={batom} alt="Batom" nome="Batom" preco={9.99} />
+                    <Item image={batom} alt="Batom" nome="Batom" preco={9.99} />
+
+                </div>
+                <div className={styles.valor}>
+                    <span>Valor Total:</span>
+                    <span>R$99,99</span>
+                </div>
+                <div className={styles.button}>
+                    <ActionButton value="Finalizar Pedido →" />
+                </div>
+            </div>
+        </>
+    )
+}
--- src/views/catalogoScreen/index.tsx
+++ src/views/catalogoScreen/index.tsx
@@ -15,18 +15,20 @@ import { Item } from '../../components/Item'
 import { MobileMenu } from '../../components/MobileMenu'
 import { CarrinhoCompras } from '../../components/CarrinhoCompras'
 import { Title } from '../../components/Title'
+import { ListaDesejos } from '../../components/ListaDesejos'
 
 
 export const CatalogoScreen = () => {
     const [menuVisible, setMenuVisible] = useState(false)
     const [carrinhoComprasVisible, setCarrinhoComprasVisible] = useState(false)
-    const [listaDesejosVisible, setlistaDesejosVisible] = useState(false)
+    const [listaDesejosVisible, setListaDesejosVisible] = useState(false)
     return (
         <div className={styles.container}>
             <header>
                 <img className={styles.logo} src={logo} alt="Logo" />
                 <MobileMenu visible={menuVisible} setVisible={setMenuVisible} />
                 <CarrinhoCompras visible={carrinhoComprasVisible} setVisible={setCarrinhoComprasVisible} />
+                <ListaDesejos visible={listaDesejosVisible} setVisible={setListaDesejosVisible} />
                 <nav>
                     <div className={styles.desktopMenu}>
                         <a href="#">Produtos</a>
@@ -36,7 +38,7 @@ export const CatalogoScreen = () => {
                     </div>
                     <LoginIcon fatherClass={`${styles.mobileMenu}`} image={menuHamburguer} alt='Menu' onClick={() => setMenuVisible(!menuVisible)} />
                     <LoginIcon fatherClass={`${styles.desktopIcon}`} image={carrinhoCompras} alt='Carrinho de Compras' onClick={() => setCarrinhoComprasVisible(!carrinhoComprasVisible)} />
-                    <LoginIcon fatherClass={`${styles.desktopIcon}`} image={listaDesejos} alt='Lista de Desejos' />
+                    <LoginIcon fatherClass={`${styles.desktopIcon}`} image={listaDesejos} alt='Lista de Desejos' onClick={() => setListaDesejosVisible(!listaDesejosVisible)} />
                 </nav>
             </header>
             <main>
