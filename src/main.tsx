dec0e99bb58bc79cf5f283ca53962ae52bd2e69c
David Galeno <davidgaleno411@gmail.com>
2023-06-01 11:54:59 -0300

    fix-erros-alerts

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 import { router } from './router/index.tsx'
 import { RouterProvider } from 'react-router-dom'
 
