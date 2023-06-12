887feb410d0e8c19e8d23d142f2e434fb8d04653
David Galeno <davidgaleno411@gmail.com>
2023-06-12 14:57:21 -0300

    fix-unused-imports

--- src/views/ItemsCatalogScreen/index.tsx
+++ src/views/ItemsCatalogScreen/index.tsx
@@ -1,9 +1,4 @@
 //Images
-import logo from '../../assets/logo.png'
-import menuHamburguer from '../../assets/menuHamburguer.svg'
-import carrinhoComprasIcon from '../../assets/carrinhoCompras.svg'
-import listaDesejosIcon from '../../assets/listaDesejos.svg'
-import { LoginIcon } from '../../components/LoginIcon'
 //CSS
 import styles from './styles.module.css'
 
@@ -11,31 +6,22 @@ import styles from './styles.module.css'
 import { useState } from 'react'
 
 //Components
-import { MobileMenu } from '../../components/MobileMenu'
-import { CarrinhoCompras } from '../../components/CarrinhoCompras'
 import { Title } from '../../components/Title'
-import { ListaDesejos } from '../../components/ListaDesejos'
 import { Item } from '../../components/Item'
-import { Link } from 'react-router-dom'
 import { useContext } from 'react'
 import { CatalogoItensContext } from '../../Context/CatalogoItensContext'
 
 //Interfaces
 import { Iitem } from '../../interfaces/Iitem'
-import { CarrinhoComprasContext } from '../../Context/CarrinhoComprasContext'
 import { Header } from '../../components/Header'
 
 
 export const ItemsCatalogScreen = () => {
 
 
-    const [menuVisible, setMenuVisible] = useState(false)
-    const [carrinhoComprasVisible, setCarrinhoComprasVisible] = useState(false)
-    const [listaDesejosVisible, setListaDesejosVisible] = useState(false)
     const [itensExibidos, setItensExibidos] = useState('Produtos')
     const { itens } = useContext(CatalogoItensContext)
 
-    const { carrinhoCompras } = useContext(CarrinhoComprasContext)
 
     return (
         <div className={styles.container}>
